923fb3b8e6d4de7f9dd153f86cc1e4eb441d0d44
wulabing <wulabing@gmail.com>
2023-04-17 19:22:56 +0800

    0.0.12
    [add] 添加订阅链接及二维码
    [fix] 更新详细使用文档，添加暴露端口变量，方便生成订阅信息

--- reality/entrypoint.sh
+++ reality/entrypoint.sh
@@ -10,6 +10,11 @@ else
     echo "UUID: $UUID"
   fi
 
+  if [ -z "$EXTERNAL_PORT" ]; then
+    echo "EXTERNAL_PORT is not set, use default value 443"
+    EXTERNAL_PORT=443
+  fi
+
   if [ -z "$DEST" ]; then
     echo "DEST is not set. default value www.apple.com:443"
     DEST="www.apple.com:443"
@@ -43,17 +48,32 @@ else
   jq ".inbounds[0].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
   jq ".inbounds[0].streamSettings.network=\"$NETWORK\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
 
+  FIRST_SERVERNAME=$(echo $SERVERNAMES | awk '{print $1}')
   # config info with green color
   echo -e "\033[32m" >/config_info.txt
   echo "IPV6: $IPV6" >>/config_info.txt
   echo "IPV4: $IPV4" >>/config_info.txt
   echo "UUID: $UUID" >>/config_info.txt
   echo "DEST: $DEST" >>/config_info.txt
+  echo "PORT: $EXTERNAL_PORT" >>/config_info.txt
   echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config_info.txt
   echo "PRIVATEKEY: $PRIVATEKEY" >>/config_info.txt
   echo "PUBLICKEY: $PUBLICKEY" >>/config_info.txt
   echo "NETWORK: $NETWORK" >>/config_info.txt
+  if [ "$IPV4" != "null" ]; then
+    SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#wulabing_docker_vless_reality_vision"
+    echo "IPV4 订阅连接: $SUB_IPV4" >>/config_info.txt
+    echo "IPV4 订阅二维码: echo $SUB_IPV4 | qrencode -o - -t UTF8" >>/config_info.txt
+  fi
+  if [ "$IPV6" != "null" ];then
+    SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#wulabing_docker_vless_reality_vision"
+    echo "IPV6 订阅连接: $SUB_IPV6" >>/config_info.txt
+    echo "IPV6 订阅二维码: echo $SUB_IPV6 | qrencode -o - -t UTF8" >>/config_info.txt
+  fi
+
+
   echo -e "\033[0m" >>/config_info.txt
+
 fi
 
 # show config info
